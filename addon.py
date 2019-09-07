43dda2c2d56f0d12519558a7a6da8f4971e00aee
leopheard <leopheard.misc@gmail.com>
2019-09-07 16:42:45 -0400

    upload

--- addon.py
+++ addon.py
@@ -4,6 +4,7 @@ from resources.lib import mainaddon
 plugin = Plugin()
 URL1 = "https://rss.art19.com/the-hartmann-report"  #HartmannReport
 URL2 = "http://stream1.thomhartmann.com/hartmannfp"  #live
+URL3 = "https://www.thomhartmann.com/podcasts.xml?token=8n5cpef8kchye42urmk7hpmpfa3oeeng" #TH HOUR
 
 @plugin.route('/')
 def main_menu():
@@ -21,6 +22,11 @@ def main_menu():
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('all_episodes'),
             'thumbnail': "https://content.production.cdn.art19.com/images/9d/86/34/c6/9d8634c6-dbca-4368-9f95-c55922195f83/460c595b8fa9ce2d24d5328a274e187c8f7ba039adc9f7b404d921e9b32092c373be11d84bde5be717e7412f277594a791eb73f996a1702e045c5fbf3b9477bd.jpeg"},
+ {
+            'label': plugin.get_string(30003), 
+            'path': plugin.url_for('all_episodes3'),
+            'thumbnail': "https://content.production.cdn.art19.com/images/9d/86/34/c6/9d8634c6-dbca-4368-9f95-c55922195f83/460c595b8fa9ce2d24d5328a274e187c8f7ba039adc9f7b404d921e9b32092c373be11d84bde5be717e7412f277594a791eb73f996a1702e045c5fbf3b9477bd.jpeg"},
+
     ]
     return items
 
@@ -36,6 +42,13 @@ def all_episodes1():
     soup1 = mainaddon.get_soup1(URL1)
     playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+
+@plugin.route('/all_episodes3/')
+def all_episodes3():
+    soup3 = mainaddon.get_soup3(URL3)
+    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
+    items = mainaddon.compile_playable_podcast3(playable_podcast3)
     return items
 
 if __name__ == '__main__':
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -9,6 +9,13 @@ def get_soup1(URL1):
     return soup1
 get_soup1("https://rss.art19.com/the-hartmann-report")
 
+def get_soup3(URL3):
+    page = requests.get(URL3)
+    soup3 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup3))
+    return soup3
+get_soup3("https://www.thomhartmann.com/podcasts.xml?token=8n5cpef8kchye42urmk7hpmpfa3oeeng")
+
 def get_playable_podcast(soup1):
     subjects = []
     for content in soup1.find_all('item'):
@@ -80,3 +87,39 @@ def compile_playable_podcast1(playable_podcast1):
             'is_playable': True,
     })
     return items
+
+def get_playable_podcast3(soup3):
+    subjects = []
+    for content in soup3.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+#            desc = content.find('description')
+#            desc = desc.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': thumbnail,
+        }
+        subjects.append(item) 
+    return subjects
+def compile_playable_podcast3(playable_podcast3):
+    items = []
+    for podcast in playable_podcast3:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+    return items
+
